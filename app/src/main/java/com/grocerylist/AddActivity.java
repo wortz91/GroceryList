ddec7131fa3de9460818c4f34a99d9ba4d0bd439
matth <geib01@email.franklin.edu>
2016-02-09 19:43:39 -0500

    Created AddActivity, AddToListActivity and LoginActivity

--- app/src/main/java/com/grocerylist/AddActivity.java
+++ app/src/main/java/com/grocerylist/AddActivity.java
@@ -1,13 +1,39 @@
 package com.grocerylist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
+//import widget classes
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Spinner;
+//import listener
+import android.widget.TextView.OnEditorActionListener;
+import android.view.View.OnClickListener;
 
-public class AddActivity extends AppCompatActivity {
+public class AddActivity extends AppCompatActivity implements OnEditorActionListener,
+OnClickListener {
+
+    //Instance variables for widgets
+    private TextView selectCategoryView;
+    private Spinner selectCategorySpinner;
+    private EditText inputItemNameEditText;
+    private EditText inputSizeWeightEditText;
+    private EditText inputBrandEditText;
+    private Button addButton;
+    private Button cancelButton;
+
+    private String inputItemNameString = "";
+    private String inputSizeWeightString = "";
+    private String inputBrandString = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +42,65 @@ public class AddActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Get references to widgets
+        selectCategoryView = (TextView) findViewById(R.id.selectCategoryTextView);
+        selectCategorySpinner = (Spinner) findViewById(R.id.selectCategorySpinner);
+        inputItemNameEditText = (EditText) findViewById(R.id.inputItemNameEditText);
+        inputSizeWeightEditText = (EditText) findViewById(R.id.inputSizeWeightEditText);
+        inputBrandEditText = (EditText) findViewById(R.id.inputBrandEditText);
+        addButton = (Button) findViewById(R.id.addButton);
+        cancelButton = (Button) findViewById(R.id.cancelButton);
+
+        //set array adapter for spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.category_array, android.R.layout.simple_spinner_item);
+
+        //set layout for spinner
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        //set adapter for spinner
+        selectCategorySpinner.setAdapter(adapter);
+
+        //Set listeners to instance variables
+        inputItemNameEditText.setOnEditorActionListener(this);
+        inputSizeWeightEditText.setOnEditorActionListener(this);
+        inputBrandEditText.setOnEditorActionListener(this);
+        addButton.setOnClickListener(this);
+        cancelButton.setOnClickListener(this);
 
     }
 
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+        if (actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_NEXT ||
+                actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+            switch (v.getId()) {
+                case R.id.inputItemNameEditText:
+                    inputItemNameString = inputItemNameEditText.getText().toString();
+                    break;
+                case R.id.inputSizeWeightEditText:
+                    inputSizeWeightString = inputSizeWeightEditText.getText().toString();
+                    break;
+                case R.id.inputBrandEditText:
+                    inputBrandString = inputBrandEditText.getText().toString();
+                    break;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addButton:
+                //insert in database
+                break;
+            case R.id.cancelButton:
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+        }
+    }
 }
---
+++ app/src/main/java/com/grocerylist/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.grocerylist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity implements TextView.OnEditorActionListener,
+        View.OnClickListener{
+
+    //instance variables for widgets
+    private EditText loginUserNameEditText;
+    private EditText loginPasswordEditText;
+    private Button loginButton;
+    private Button loginCancelButton;
+
+    private String loginUserNameString = "";
+    private String loginPasswordString = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        loginUserNameEditText = (EditText) findViewById(R.id.userNameEditText);
+        loginPasswordEditText = (EditText) findViewById(R.id.passwordEditText);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginCancelButton = (Button) findViewById(R.id.loginCancelButton);
+
+        loginUserNameEditText.setOnEditorActionListener(this);
+        loginPasswordEditText.setOnEditorActionListener(this);
+        loginButton.setOnClickListener(this);
+        loginCancelButton.setOnClickListener(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+        if (actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_NEXT ||
+                actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+            switch (v.getId()) {
+                case R.id.inputItemNameEditText:
+                    loginUserNameString = loginUserNameEditText.getText().toString();
+                    break;
+                case R.id.inputSizeWeightEditText:
+                    loginPasswordString = loginPasswordEditText.getText().toString();
+                    break;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginButton:
+                //check user name and password
+                break;
+            case R.id.cancelButton:
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+    }
+}
